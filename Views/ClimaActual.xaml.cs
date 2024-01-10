eea3913e4656dfadec3d77ea9d00504278c04aeb
Juan Andres Alvarez Pullas <juan.alvarez.pullas@udla.edu.ec>
2024-01-10 08:56:58 -0500

    Metodo Clicked terminado

--- Views/ClimaActual.xaml.cs
+++ Views/ClimaActual.xaml.cs
@@ -1,3 +1,7 @@
+using Alvarez_EjemploAPI.Model;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
 namespace Alvarez_EjemploAPI.Views;
 
 public partial class ClimaActual : ContentPage
@@ -7,8 +11,27 @@ public partial class ClimaActual : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
+		string latitud = lat.Text;
+		string longitud = lon.Text;
+
+		if(Connectivity.NetworkAccess == NetworkAccess.Internet)
+		{
+			using (var client = new HttpClient())
+			{
+				string url = $"https://api.openweathermap.org/data/2.5/weather?lat=" + latitud + "&lon=" + longitud + "&appid=486cac8b9c205ff94d6c8895944c74ae\r\n";
+				var response =  await client.GetAsync(url);
+				if (response.IsSuccessStatusCode)
+				{
+					var json  = await response.Content.ReadAsStringAsync();
+					var clima = JsonConvert.DeserializeObject<Rootobject>(json);
+
+					weatherLabel.Text = clima.weather[0].main;
+				}
+
+			}
+		}
 
     }
 }
